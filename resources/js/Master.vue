8222b357653f32cedbb06abcd8fd80ad483690ec
hisyammaulana <hisyammaulana100@gmail.com>
2019-10-07 00:25:02 +0700

    master template & route in vue

---
+++ resources/js/Master.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="wrapper">
+    <app-navbar />
+
+    <app-sidebar />
+
+    <div class="content-page">
+      <div class="content">
+        <router-view></router-view>
+        <app-footer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { mapState, mapGetters } from "vuex";
+import Navbar from "./components/NavbarComponent.vue";
+import Sidebar from "./components/SidebarComponent.vue";
+import Footer from "./components/FooterComponent.vue";
+export default {
+  components: {
+    "app-navbar": Navbar,
+    "app-sidebar": Sidebar,
+    "app-footer": Footer
+  }
+};
+</script>
---
+++ resources/js/router.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './pages/Home.vue'
+import Members from './pages/members/Member.vue'
+import Events from './pages/event/IndexEvents.vue'
+import Presences from './pages/presences/IndexPresence.vue'
+import Login from './pages/Login.vue'
+import store from './store.js'
+
+
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/members',
+            name: 'members',
+            component: Members
+        },
+        {
+            path: '/events',
+            name: 'events',
+            component: Events
+        },
+        {
+            path: '/presence',
+            name: 'presence',
+            component: Presences
+        }
+    ]
+})
+export default router
+
