ce1ae327bc42beedd42f794aab65cf920666d5ba
hisyammaulana <hisyammaulana100@gmail.com>
2019-10-08 15:36:50 +0700

    add event page

---
+++ resources/js/pages/event/Events.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6 col-xl-3">
+        <!-- Simple card -->
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="template/images/gallery/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make
+              up the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-xl-3">
+        <!-- Simple card -->
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="template/images/gallery/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make
+              up the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <!-- Simple card -->
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="template/images/gallery/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make
+              up the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <!-- Simple card -->
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="template/images/gallery/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make
+              up the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <!-- Simple card -->
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="template/images/gallery/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make
+              up the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <!-- Simple card -->
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="template/images/gallery/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make
+              up the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <!-- Simple card -->
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="template/images/gallery/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make
+              up the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-xl-3">
+        <!-- Simple card -->
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="template/images/gallery/1.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make
+              up the bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Button</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
